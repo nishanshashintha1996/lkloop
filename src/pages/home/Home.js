725baed06901ba55bb18eb8695d96d25d0491df2
nishanshashintha1996 <shashinthanishan00@gmail.com>
2023-03-05 22:29:38 +0530

    Dev 11

---
+++ src/pages/home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Home.css'
+
+export default function Home() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
